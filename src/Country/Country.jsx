b00ccfa4fe9bed02377b4df6cfdb4843f37c95fd
Mashnur <221007712@eastdelta.edu.bd>
2024-03-31 03:39:19 +0600

    added component jsx ,eventhandler, state, useeffect and also fetch from api

---
+++ src/Country/Country.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+import './Country.css'
+
+const Country = ({country,handlevisitecountry}) => {
+    const {name, flags, population, area, cca3 }=country
+    const[visited,setVisited]=useState(false)
+   const handleButton=()=>{
+    setVisited(!visited)
+   }
+//    console.log(handlevisitecountry)
+const passparams =()=>handlevisitecountry(country)
+   
+    return (
+        <div className={`Country ${visited && `visited`}`}>
+            <h3 style={{color:visited?'purple':'green'}}>Name:{name?.common}</h3>
+            <img src={flags.png} alt="" />
+            <p>{population}</p>
+            <p>{area}</p>
+            <p>Code:{cca3}</p>
+            <button onClick={passparams}>MArk As visited</button><br />
+            <button onClick={handleButton}>Visited</button> 
+            { visited? 'I have visited this country':'i will go'}
+            
+           
+            
+        </div>
+    );
+};
+
+export default Country;
---
+++ src/components/Countries/Countries.jsx
@@ -0,0 +1,45 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import Country from "../../Country/Country";
+import './Countries.css'
+
+
+
+
+const Countries = () => {
+    const [countries, SetCountries]= useState([])
+    const [visited,setVisited]=useState([])
+    useEffect(()=>{
+        fetch('https://restcountries.com/v3.1/all')
+        .then(res=>res.json())
+        .then(data=>SetCountries(data))
+    },[])
+    const handleVisited =country =>{
+        console.log('my visited list')
+        const Newvisited=[...visited,country]
+        setVisited(Newvisited)
+    }
+    return (
+        <div >
+            <h3>Countries: {countries.length}</h3>
+            <div>
+                <h5>Visited Country:{visited.length}</h5>
+                <ul>
+                    {
+                        visited.map(country=> <li key={country.cca3}>{country.name.common}</li>)
+                    }
+
+                </ul>
+            </div>
+            <div className="country-container">
+            {
+              countries.map(country=><Country key={country.cca3}country={country} handlevisitecountry={handleVisited} ></Country>)
+              //because of optimization issue we use a key 
+            }
+            
+            </div>
+        </div>
+    );
+};
+
+export default Countries;
